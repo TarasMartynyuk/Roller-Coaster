114b4f80a3ec35e7fd19bb9fe48746ed6bad3637
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2018-06-05 00:50:34 +0300

    downloaded code from codngame

---
+++ roller_coaster.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+    int L;
+    int C;
+    int N;
+    cin >> L >> C >> N; cin.ignore();
+    for (int i = 0; i < N; i++) {
+        int Pi;
+        cin >> Pi; cin.ignore();
+    }
+
+    // Write an action using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "answer" << endl;
+}
